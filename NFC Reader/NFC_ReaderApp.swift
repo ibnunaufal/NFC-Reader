e5cf7dd9091f8f18d1c476fa20ac6c1e1c45599f
ibnu naufal <ibnunaufal5758@gmail.com>
2023-04-13 08:19:53 +0700

    Initial Commit

---
+++ NFC Reader/NFC_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFC_ReaderApp.swift
+//  NFC Reader
+//
+//  Created by Macbook Pro on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct NFC_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
